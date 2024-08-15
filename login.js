8c423670dd68e143c340703f3f0149c1fc0d9ebe
AbielOmar <163035894+AbielOmar@users.noreply.github.com>
2024-08-15 08:37:15 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+function loguear()
+{
+
+var user= document.getElementById("usuario").value;
+
+var pass= document.getElementById("pass").value;
+
+if(user=="Promesa" && pass=="Taquito")
+
+{
+
+
+window.location="inicio.html";
+
+}
+else{
+    alert("Estas mal mi amor :(")
+}
+
+
+}
